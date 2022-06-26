84ba6c139fb3fefe22a07eafb53bc0303199c2c5
Felipe Neves Ricardo <contato@felipenr.com>
2022-06-26 03:47:05 -0300

    improve Lesson and Sidebar components

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,19 +1,47 @@
+import { gql, useQuery } from "@apollo/client";
 import { Lesson } from "./Lesson";
-
 export function Sidebar() {
+    const {data} = useQuery<Sidebar.GetLessonsQueryResponse>(GET_LESSONS_QUERY)
     return(
         <aside className="w-[21.75rem] bg-gray-700 border-l p-6 border-gray-600">
             <span className="font-bold text-2xl pb-6 mb-6 border-gray-500 block">
             Cronograma de aulas
             </span>
             <div className="flex flex-col gap-8">
-                <Lesson/>
-                <Lesson/>
-                <Lesson/>
-                <Lesson/>
-                <Lesson/>
-                <Lesson/>
+                {data?.lessons.map(lesson => 
+                <Lesson
+                key={lesson.id}
+                title={lesson.title}
+                slug={lesson.slug}
+                availableAt={new Date(lesson.availableAt)}
+                type={lesson.lessonType}
+                />)}
+                
             </div>
         </aside>
     ) 
+}
+
+const GET_LESSONS_QUERY = gql`
+query {
+  lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+    id
+    lessonType
+    availableAt
+    title
+    slug
+  }
+}
+`
+
+namespace Sidebar {
+    export interface GetLessonsQueryResponse {
+        lessons: {
+            id: string
+            title: string
+            slug: string
+            availableAt: string
+            lessonType: 'live' | 'class'
+        }[]
+    }
 }
