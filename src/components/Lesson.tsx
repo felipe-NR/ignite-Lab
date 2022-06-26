70bfb1212e6525126a3df5dc5cffe02f922e0b06
Felipe Neves Ricardo <contato@felipenr.com>
2022-06-26 06:57:33 -0300

    Add routing and improve Video component

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,17 +1,18 @@
 import { format, isPast } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 import { CheckCircle, Lock } from 'phosphor-react'
+import { Link } from 'react-router-dom'
 
 export function Lesson(props:Lesson.Props) {
     const isLessonAvailable = isPast(props.availableAt)
     const availableDateFormatted = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", { locale: ptBR })
 
     return(
-        <a href="#">
+        <Link to={`/event/lesson/${props.slug}`} className="group">
             <span className="text-gray-300">
             {availableDateFormatted}
             </span>
-            <div className="rounded border border-gray-500 p-4 mt-2">
+            <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
                 <header className="flex items-center justify-between"> 
                     {isLessonAvailable ? (
                         <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
@@ -34,7 +35,7 @@ export function Lesson(props:Lesson.Props) {
                 {props.title}
                 </strong>
             </div>
-        </a>
+        </Link>
     )
 }
 
